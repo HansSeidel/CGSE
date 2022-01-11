94a8baf3c57405e6ccaa0b5eeeb9b160d7a03d84
Hans Seidel <hans.j.seidel@gmail.com>
2022-01-11 19:34:22 +0100

    Learning - Create own shaders

--- CGSE-Final/src/Application.cpp
+++ CGSE-Final/src/Application.cpp
@@ -1,4 +1,46 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+#include <iostream>
+
+/*H-No idea what is happening here. My C++ and knowlegde about shaders is to far back for this*/
+static unsigned int CompileShader(unsigned int type, const std::string& source) {
+    unsigned int id = glCreateShader(type);
+    const char* SRC = source.c_str();
+    glShaderSource(id, 1, &SRC, nullptr);
+    glCompileShader(id);
+
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    if (result == GL_FALSE) {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*) alloca(length * sizeof(char));
+        glGetShaderInfoLog(id, length, &length, message);
+        std::cout << "Failed to compile " << (type== GL_VERTEX_SHADER? "vertex" : "fragment") << " shader. Message: " << message << std::endl;
+        glDeleteShader(id);
+        return 0;
+    }
+
+    return id;
+}
+/*H-No idea what is happening here. My C++ and knowlegde about shaders is to far back for this*/
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader) {
+    unsigned int program = glCreateProgram();
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
+}
+
 
 int main(void)
 {
@@ -19,17 +61,55 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    /* H-Initialize glew Init*/
+    glewInit();
+
+    /* H-Create a VertexBuffer */
+    //H-Create Data-Array for an triangle
+    float triangle[6] = {
+        -0.5f, -0.5f,
+        0.0f,   0.5f,
+        0.5f,  -0.5f
+    };
+
+    unsigned int buffer;                    
+    glGenBuffers(1, &buffer);
+    //H-Create a vertex buffer and save the id inside the memory address of the buffer variable
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    //H-Select the generated Buffer to edit
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(triangle), triangle, GL_STATIC_DRAW);
+
+    
+    //H-Rewatch: https://www.youtube.com/watch?v=x0H--CL2tUI&list=PLlrATfBNZ98foTJPJ_Ev03o2oq3-GGOS2&index=5 to understand glVertexAttribPointer.
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0); 
+
+    //H-Write own Vertex Shader
+    std::string vertexShader = 
+        "#version 330 core\n"
+        "layout(location = 0) in vec4 position;\n"
+        "void main(){\n"
+        "gl_Position = position;\n"
+        "}";
+    //H-Write own Fragment Shader
+    std::string fragmentShader =
+        "#version 330 core\n"
+        "layout(location = 0) out vec4 color;\n"
+        "void main(){\n"
+        "color = vec4(1.0,0.0,0.0,1.0);\n"
+        "}";
+
+    unsigned int shader = CreateShader(vertexShader, fragmentShader);
+    glUseProgram(shader);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f(0.0f, 0.5f);
-        glVertex2f(0.5f, -0.5f);
-        glEnd();
+        /*H_Draw the vertex buffer*/
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
@@ -37,6 +117,8 @@ int main(void)
         /* Poll for and process events */
         glfwPollEvents();
     }
+
+    glDeleteShader(shader);
 
     glfwTerminate();
     return 0;
--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 #Final project CGSE
 
 > ***Author:*** Hans Seidel (if20b211) <br>
-> *Version:* 0.0 (gitTag: v0.0) <br>
-> *Git commit message:* Initial Commit <br>
+> *Version:* 0.2 (gitTag: v0.2) <br>
+> *Git commit message:* Learning - Create own shaders <br>
 > *Version Date:* 11.01.2022 
 
 
@@ -10,6 +10,10 @@
 The aim of this project is to learn openGL and genereate a demonstration project. 
 This project is part of a lecture at the *Fachhochschule Technikum Wien*.
 The final project shows a endless world with the same house arrayed next to each other.
+
+**In order to setup the project you only need the follwing chapters:**
+- [Envirement and Dependencies](#envirement-and-dependencies)
+- [Setup](#setup)
 
 ##Tasks
 The original assignment description is shown at the following link: [Project Submission](https://moodle.technikum-wien.at/mod/assign/view.php?id=850504) *(You need permissions for this link)*
@@ -39,11 +43,27 @@ The original assignment description is shown at the following link: [Project Sub
 > - lib-vc2019 
 > - include
 
+> ***Glew:*** Windows 32-bit and 64-bit <br>
+> *Version:* 2.1.0
+
 ##Setup
 1. Download the ***GLFW Library*** and put the *include* and the *lib-vc2019* folder inside the *Dependencies/GLFW* folder.
+2. Download the ***Glew Library*** and put the whole content into the **Dependencies/GLEW* folder.
 
 
 ## Documentation
-(*v0.0)
+The documentation is split into two parts. 
+The first part descripes the project(s) i used to repeat deepen my knowlegde about openGl.
+This part follows the steps of [this](https://www.youtube.com/watch?v=W3gAzLwfIP0&list=PLlrATfBNZ98foTJPJ_Ev03o2oq3-GGOS2&index=1) tutorial-series.
+<br>
+The second part descripes the progress of the final project.
+<br>
+In git the first part is below version 1.0. So any tag with (v0.*)
+
+### Repetition and consolidation
+**(v0.0)** -
 At the beginning of the project, the setup has been created. In order to check if everything is running 
 correct, a simple triangle was created.
+**(v0.1)** - The next step was to write our own shaders and get the program running again. 
+Within the fragment shader we defined the color of the triangle.
+
